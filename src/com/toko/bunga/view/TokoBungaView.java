5cbe678307b254332a9f765ccf26139e54393271
LiaListriani <lia.listriani2001@gmail.com>
2022-11-13 13:45:44 +0700

    Pertemuan 4

--- src/com/toko/bunga/view/TokoBungaView.java
+++ src/com/toko/bunga/view/TokoBungaView.java
@@ -5,17 +5,22 @@
  */
 package com.toko.bunga.view;
 
+import com.toko.bunga.controller.TokoBungaController;
+
 /**
  *
  * @author LilianXx
  */
 public class TokoBungaView extends javax.swing.JFrame {
+    public final TokoBungaController tokoBungaController = new TokoBungaController(this);
+    
 
     /**
      * Creates new form TokoBungaView
      */
     public TokoBungaView() {
         initComponents();
+        tokoBungaController.getAllData();
     }
 
     /**
@@ -36,13 +41,13 @@ public class TokoBungaView extends javax.swing.JFrame {
         txtNama = new javax.swing.JTextField();
         txtJenis = new javax.swing.JTextField();
         txtHarga = new javax.swing.JTextField();
-        btn_save = new javax.swing.JButton();
         btn_update = new javax.swing.JButton();
         btn_delete = new javax.swing.JButton();
         btn_clear = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblBunga = new javax.swing.JTable();
         jLabel5 = new javax.swing.JLabel();
+        btn_save = new javax.swing.JButton();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -71,16 +76,19 @@ public class TokoBungaView extends javax.swing.JFrame {
             }
         });
 
-        btn_save.setText("Save");
-        btn_save.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_saveActionPerformed(evt);
+        btn_update.setText("Update");
+        btn_update.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_updateMouseClicked(evt);
             }
         });
 
-        btn_update.setText("Update");
-
         btn_delete.setText("Delete");
+        btn_delete.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_deleteMouseClicked(evt);
+            }
+        });
         btn_delete.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_deleteActionPerformed(evt);
@@ -88,6 +96,11 @@ public class TokoBungaView extends javax.swing.JFrame {
         });
 
         btn_clear.setText("Clear");
+        btn_clear.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_clearMouseClicked(evt);
+            }
+        });
         btn_clear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_clearActionPerformed(evt);
@@ -105,10 +118,22 @@ public class TokoBungaView extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tblBunga.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblBungaMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tblBunga);
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel5.setText("Toko Bunga");
+
+        btn_save.setText("Save");
+        btn_save.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_saveMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -137,8 +162,8 @@ public class TokoBungaView extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(49, 49, 49)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(btn_save, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btn_delete, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(btn_delete, javax.swing.GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE)
+                            .addComponent(btn_save, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(42, 42, 42)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btn_update, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -175,8 +200,8 @@ public class TokoBungaView extends javax.swing.JFrame {
                             .addComponent(txtHarga, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(35, 35, 35)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(btn_save)
-                            .addComponent(btn_update))
+                            .addComponent(btn_update)
+                            .addComponent(btn_save))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(btn_delete)
@@ -191,10 +216,6 @@ public class TokoBungaView extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtKodeActionPerformed
 
-    private void btn_saveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_saveActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btn_saveActionPerformed
-
     private void btn_deleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_deleteActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_deleteActionPerformed
@@ -202,6 +223,31 @@ public class TokoBungaView extends javax.swing.JFrame {
     private void btn_clearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_clearActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_clearActionPerformed
+
+    private void btn_deleteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_deleteMouseClicked
+        // TODO add your handling code here:
+        tokoBungaController.deleteBunga();
+    }//GEN-LAST:event_btn_deleteMouseClicked
+
+    private void btn_updateMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_updateMouseClicked
+        // TODO add your handling code here:
+        tokoBungaController.updateBunga();
+    }//GEN-LAST:event_btn_updateMouseClicked
+
+    private void btn_clearMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_clearMouseClicked
+        // TODO add your handling code here:
+        tokoBungaController.clear();
+    }//GEN-LAST:event_btn_clearMouseClicked
+
+    private void tblBungaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblBungaMouseClicked
+        // TODO add your handling code here:
+        tokoBungaController.getdata();
+    }//GEN-LAST:event_tblBungaMouseClicked
+
+    private void btn_saveMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_saveMouseClicked
+        // TODO add your handling code here:
+        tokoBungaController.saveBunga();
+    }//GEN-LAST:event_btn_saveMouseClicked
 
     /**
      * @param args the command line arguments
@@ -256,4 +302,39 @@ public class TokoBungaView extends javax.swing.JFrame {
     private javax.swing.JTextField txtKode;
     private javax.swing.JTextField txtNama;
     // End of variables declaration//GEN-END:variables
+
+    /**
+     * @return the tblBunga
+     */
+    public javax.swing.JTable getTblBunga() {
+        return tblBunga;
+    }
+
+    /**
+     * @return the txtHarga
+     */
+    public javax.swing.JTextField getTxtHarga() {
+        return txtHarga;
+    }
+
+    /**
+     * @return the txtJenis
+     */
+    public javax.swing.JTextField getTxtJenis() {
+        return txtJenis;
+    }
+
+    /**
+     * @return the txtKode
+     */
+    public javax.swing.JTextField getTxtKode() {
+        return txtKode;
+    }
+
+    /**
+     * @return the txtNama
+     */
+    public javax.swing.JTextField getTxtNama() {
+        return txtNama;
+    }
 }
